68ae54e8941e7cb098c158c1686160ff735e69d1
Neeraj Kumar <nraj_adi@yahoo.co.in>
2019-06-08 14:05:28 +0530

    import export example is added.

---
+++ second/one.js
@@ -0,0 +1,11 @@
+function add(x, y) {
+    return x+y;
+}
+function subtract(x, y) {
+    return x-y;
+}
+// exporting add method
+module.exports.add = add;
+exports.subtract = subtract;
+
+// console.log(module);
---
+++ second/two.js
@@ -0,0 +1,8 @@
+// importing functions of one.js
+const f = require('./one');
+console.log('sum of 5 & 6 is ');
+let a = f.add(5,6);
+console.log(a);
+console.log('diff of 15 & 6 is ');
+let d = f.subtract(15,6);
+console.log(d);
